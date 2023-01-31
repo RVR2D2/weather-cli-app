75c9576e482ca56f3726c984c2cde20fc5286c7b
Vadim Redkin <59052910+RVR2D2@users.noreply.github.com>
2023-01-31 17:38:13 +0600

    Работа с файловой системой

--- weather.js
+++ weather.js
@@ -3,11 +3,22 @@ import {
   getArgs
 } from './helpers/args.js'
 import {
-  printHelp
+  printHelp,
+  printSuccess,
+  printError
 } from "./services/log.service.js";
 import {
   saveKeyValue
 } from "./services/storage.service.js";
+
+const saveToken = async (token) => {
+  try {
+    await saveKeyValue('token', token)
+    printSuccess('Токен сохранен')
+  }catch (e) {
+    printError(e.message)
+  }
+}
 
 const initCLI = () => {
   const args = getArgs(process.argv)
@@ -19,7 +30,7 @@ const initCLI = () => {
   }
 
   if (args.t) {
-    saveKeyValue('token', args.t)
+    return saveToken(args.t);
   }
 
   // Вывести погоду
